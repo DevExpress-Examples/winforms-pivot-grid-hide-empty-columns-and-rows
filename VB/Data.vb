253eb7e30df83deab40eeec068f8b3c95d17564c
DevExpressExampleBot <fakeEmail>
2022-10-11 16:43:52 +0400

    Source auto update [skip ci]

--- VB/Data.vb
+++ VB/Data.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to hide empty Field Values (Columns/Rows)
 ' 
 ' It is necessary to use the PivotGridControl.CustomFieldValueCells
@@ -8,7 +8,6 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E2807
-
 ' Developer Express Code Central Example:
 ' How to hide particular rows and columns
 ' 
@@ -20,83 +19,91 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E2769
-
-
-Imports Microsoft.VisualBasic
 Imports System.Data
 Imports DevExpress.XtraPivotGrid
 
 Namespace XtraPivotGrid_HidingColumnsAndRows
-	Public NotInheritable Class PivotHelper
-		Public Const Employee As String = "Employee"
-		Public Const Widget As String = "Widget"
-		Public Const Month As String = "Month"
-		Public Const RetailPrice As String = "Retail Price"
-		Public Const WholesalePrice As String = "Wholesale Price"
-		Public Const Quantity As String = "Quantity"
-		Public Const Remains As String = "Remains"
 
-		Public Const EmployeeA As String = "Employee A"
-		Public Const EmployeeB As String = "Employee B"
-		Public Const WidgetA As String = "Widget A"
-		Public Const WidgetB As String = "Widget B"
-		Public Const WidgetC As String = "Widget C"
+    Public Module PivotHelper
 
-		Private Sub New()
-		End Sub
-		Public Shared Sub FillPivot(ByVal pivot As PivotGridControl)
-			pivot.Fields.Add(Employee, PivotArea.RowArea)
-			pivot.Fields.Add(Widget, PivotArea.RowArea)
-			pivot.Fields.Add(Month, PivotArea.ColumnArea).AreaIndex = 0
-			pivot.Fields.Add(RetailPrice, PivotArea.DataArea)
-			pivot.Fields.Add(WholesalePrice, PivotArea.DataArea)
-			pivot.Fields.Add(Quantity, PivotArea.DataArea)
-			pivot.Fields.Add(Remains, PivotArea.DataArea)
-			'foreach (PivotGridField field in pivot.Fields) {
-			'    field.AllowedAreas = GetAllowedArea(field.Area);
-			'}
-			pivot.OptionsView.RowTotalsLocation = PivotRowTotalsLocation.Far
-			pivot.OptionsView.ColumnTotalsLocation = PivotTotalsLocation.Far
-			pivot.OptionsDataField.Area = PivotDataArea.ColumnArea
-			pivot.OptionsDataField.AreaIndex = 1
-		End Sub
-		Private Shared Function GetAllowedArea(ByVal area As PivotArea) As PivotGridAllowedAreas
-			Select Case area
-				Case PivotArea.ColumnArea
-					Return PivotGridAllowedAreas.ColumnArea
-				Case PivotArea.RowArea
-					Return PivotGridAllowedAreas.RowArea
-				Case PivotArea.DataArea
-					Return PivotGridAllowedAreas.DataArea
-				Case PivotArea.FilterArea
-					Return PivotGridAllowedAreas.FilterArea
-				Case Else
-					Return PivotGridAllowedAreas.All
-			End Select
-		End Function
-		Public Shared Function GetDataTable() As DataTable
-			Dim table As New DataTable()
-			table.Columns.Add(Employee, GetType(String))
-			table.Columns.Add(Widget, GetType(String))
-			table.Columns.Add(Month, GetType(Integer))
-			table.Columns.Add(RetailPrice, GetType(Double))
-			table.Columns.Add(WholesalePrice, GetType(Double))
-			table.Columns.Add(Quantity, GetType(Integer))
-			table.Columns.Add(Remains, GetType(Integer))
-			table.Rows.Add(EmployeeA, WidgetA, 6, 45.6, 0, 3, 0)
-			table.Rows.Add(EmployeeA, WidgetA, 7, 38.9, 0, 6, 1)
-			table.Rows.Add(EmployeeA, WidgetB, 6, 24.7, 0, 7, 0)
-			table.Rows.Add(EmployeeA, WidgetB, 7, 8.3, 0, 5, 1)
-			table.Rows.Add(EmployeeA, WidgetC, 6, 10.0, 0, 4, 0)
-			table.Rows.Add(EmployeeA, WidgetC, 7, 20.0, 0, 5, 1)
-			table.Rows.Add(EmployeeB, WidgetA, 6, 77.8, 0, 2, 0)
-			table.Rows.Add(EmployeeB, WidgetA, 7, 32.5, 0, 1, 1)
-			table.Rows.Add(EmployeeB, WidgetB, 6, 12, 0, 0, 0)
-			table.Rows.Add(EmployeeB, WidgetB, 7, 6.7, 5, 4, 1)
-			table.Rows.Add(EmployeeB, WidgetC, 6, 0, 0, 0, 0)
-			table.Rows.Add(EmployeeB, WidgetC, 7, 0, 0, 0, 0)
-			Return table
-		End Function
-	End Class
+        Public Const Employee As String = "Employee"
 
+        Public Const Widget As String = "Widget"
+
+        Public Const Month As String = "Month"
+
+        Public Const RetailPrice As String = "Retail Price"
+
+        Public Const WholesalePrice As String = "Wholesale Price"
+
+        Public Const Quantity As String = "Quantity"
+
+        Public Const Remains As String = "Remains"
+
+        Public Const EmployeeA As String = "Employee A"
+
+        Public Const EmployeeB As String = "Employee B"
+
+        Public Const WidgetA As String = "Widget A"
+
+        Public Const WidgetB As String = "Widget B"
+
+        Public Const WidgetC As String = "Widget C"
+
+        Public Sub FillPivot(ByVal pivot As PivotGridControl)
+            pivot.Fields.Add(Employee, PivotArea.RowArea)
+            pivot.Fields.Add(Widget, PivotArea.RowArea)
+            pivot.Fields.Add(Month, PivotArea.ColumnArea).AreaIndex = 0
+            pivot.Fields.Add(RetailPrice, PivotArea.DataArea)
+            pivot.Fields.Add(WholesalePrice, PivotArea.DataArea)
+            pivot.Fields.Add(Quantity, PivotArea.DataArea)
+            pivot.Fields.Add(Remains, PivotArea.DataArea)
+            'foreach (PivotGridField field in pivot.Fields) {
+            '    field.AllowedAreas = GetAllowedArea(field.Area);
+            '}
+            pivot.OptionsView.RowTotalsLocation = PivotRowTotalsLocation.Far
+            pivot.OptionsView.ColumnTotalsLocation = PivotTotalsLocation.Far
+            pivot.OptionsDataField.Area = PivotDataArea.ColumnArea
+            pivot.OptionsDataField.AreaIndex = 1
+        End Sub
+
+        Private Function GetAllowedArea(ByVal area As PivotArea) As PivotGridAllowedAreas
+            Select Case area
+                Case PivotArea.ColumnArea
+                    Return PivotGridAllowedAreas.ColumnArea
+                Case PivotArea.RowArea
+                    Return PivotGridAllowedAreas.RowArea
+                Case PivotArea.DataArea
+                    Return PivotGridAllowedAreas.DataArea
+                Case PivotArea.FilterArea
+                    Return PivotGridAllowedAreas.FilterArea
+                Case Else
+                    Return PivotGridAllowedAreas.All
+            End Select
+        End Function
+
+        Public Function GetDataTable() As DataTable
+            Dim table As DataTable = New DataTable()
+            table.Columns.Add(Employee, GetType(String))
+            table.Columns.Add(Widget, GetType(String))
+            table.Columns.Add(Month, GetType(Integer))
+            table.Columns.Add(RetailPrice, GetType(Double))
+            table.Columns.Add(WholesalePrice, GetType(Double))
+            table.Columns.Add(Quantity, GetType(Integer))
+            table.Columns.Add(Remains, GetType(Integer))
+            table.Rows.Add(EmployeeA, WidgetA, 6, 45.6, 0, 3, 0)
+            table.Rows.Add(EmployeeA, WidgetA, 7, 38.9, 0, 6, 1)
+            table.Rows.Add(EmployeeA, WidgetB, 6, 24.7, 0, 7, 0)
+            table.Rows.Add(EmployeeA, WidgetB, 7, 8.3, 0, 5, 1)
+            table.Rows.Add(EmployeeA, WidgetC, 6, 10.0, 0, 4, 0)
+            table.Rows.Add(EmployeeA, WidgetC, 7, 20.0, 0, 5, 1)
+            table.Rows.Add(EmployeeB, WidgetA, 6, 77.8, 0, 2, 0)
+            table.Rows.Add(EmployeeB, WidgetA, 7, 32.5, 0, 1, 1)
+            table.Rows.Add(EmployeeB, WidgetB, 6, 12, 0, 0, 0)
+            table.Rows.Add(EmployeeB, WidgetB, 7, 6.7, 5, 4, 1)
+            table.Rows.Add(EmployeeB, WidgetC, 6, 0, 0, 0, 0)
+            table.Rows.Add(EmployeeB, WidgetC, 7, 0, 0, 0, 0)
+            Return table
+        End Function
+    End Module
 End Namespace
